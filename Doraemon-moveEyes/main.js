215a71a68162cce429cbb65371fdaa0e86d45f8f
shenfeng1945 <823902826@qq.com>
2017-06-17 14:37:01 +0800

    let

--- Doraemon-moveEyes/main.js
+++ Doraemon-moveEyes/main.js
@@ -1,11 +1,12 @@
 /**
  * Created by lzc on 2017/6/17.
  */
-var  eye1 = document.querySelector('.eye1_circle')
-var  eye2 = document.querySelector('.eye2_circle')
-setInterval(function () {
-   var degreeX = parseInt(Math.random()*90-45,10)
-   var degreeY = parseInt(Math.random()*90-45,10)
-    eye1.style.transform = 'rotateX('+degreeX+'deg) rotateY('+degreeY+'deg)'
-    eye2.style.transform = 'rotateX('+degreeX+'deg) rotateY('+degreeY+'deg)'
-},800)
+
+    let eye1 = document.querySelector('.eye1_circle')
+    let eye2 = document.querySelector('.eye2_circle')
+    setInterval(function () {
+        let degreeX = parseInt(Math.random() * 90 - 45, 10)
+        let degreeY = parseInt(Math.random() * 90 - 45, 10)
+        eye1.style.transform = 'rotateX(' + degreeX + 'deg) rotateY(' + degreeY + 'deg)'
+        eye2.style.transform = 'rotateX(' + degreeX + 'deg) rotateY(' + degreeY + 'deg)'
+    }, 800)
