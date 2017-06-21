1aef2b875ab68f9a1be197cfd385dae5b39d402c
shenfeng1945 <823902826@qq.com>
2017-06-22 07:27:25 +0800

    OK

--- Tabs组件/main.js
+++ Tabs组件/main.js
@@ -1,10 +1,10 @@
 /**
  * Created by lzc on 2017/6/20.
  */
-$('.out .tabs').on('click','.out .tabs li',function (e) {
+$('.out .tabs').on('click','li',function (e) {
     let li = e.currentTarget
     let $li = $(li)
     let index = $li.index()
     $li.addClass('active').siblings('.active').removeClass('active')
-    $li.parent().next().children().eq(index).addClass('active').siblings().removeClass('active')
+    $li.parent('.tabs').next().children().eq(index).addClass('active').siblings('.active').removeClass('active')
 })
