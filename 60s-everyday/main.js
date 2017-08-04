ac3b74dafeadbfb7806ef48fa5cd62ff72bbee54
shenfeng1945 <823902826@qq.com>
2017-08-04 21:50:10 +0800

    first

---
+++ 60s-everyday/main.js
@@ -0,0 +1,13 @@
+$('.container .play').on('click', function(e) {
+  let $play = $(e.currentTarget)
+  let $line = $play.closest('.container').find('.move-line')
+  if ($play.hasClass('active')) {
+    $play.removeClass('active')
+     $line.removeClass('active')
+  } else {
+    $play.addClass('active')
+     $line.addClass('active')
+  }
+  let number = 0
+  $('.start-time').text(number)
+})
